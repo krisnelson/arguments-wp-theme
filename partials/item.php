0e469ce6fb89c6fd3f630c7942ea106a00581fac
Kristopher <krisnelson@gmail.com>
2017-03-29 06:30:16 +0000

    Minor typo edit.

--- partials/item.php
+++ partials/item.php
@@ -27,7 +27,6 @@
 				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'arguments' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
-
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'arguments' ),
 				'after'  => '</div>',
